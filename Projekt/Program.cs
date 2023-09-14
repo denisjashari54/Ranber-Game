22de596d41902704496f8f62ff76e5cf71f7d65f
Densi Jashari <107101355+denisjashari54@users.noreply.github.com>
2023-09-14 14:00:14 +0200

    Create Program.cs

---
+++ Projekt/Program.cs
@@ -0,0 +1,11 @@
+namespace RanberGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MainMenu gameManager = new MainMenu();
+            gameManager.Run();
+        }
+    }
+}
